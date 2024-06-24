1fb4d0ee66f3d6d7bb37b382d30514e265cd532b
Anne Murakaru <anne.murakaru@gmail.com>
2024-06-24 17:54:57 +0200

    Update Reverse.py

--- Reverse.py
+++ Reverse.py
@@ -1,6 +1,5 @@
-Write a program that will reverse a four digit number.Also it checks whether the reverse
+#Write a program that will reverse a four digit number.Also it checks whether the reverse
 
-Step 1 
 
 x = input("Enter 4 digit number: ")
 y = x[::-1]
